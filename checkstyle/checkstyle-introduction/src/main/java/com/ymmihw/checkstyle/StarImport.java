7b6f3325dc82f67bf8c0c90e52e527cdc298c4d8
whimmy <whimmy@126.com>
2018-06-09 14:10:48 +0800

    add sample

---
+++ checkstyle/checkstyle-introduction/src/main/java/com/ymmihw/checkstyle/StarImport.java
@@ -0,0 +1,9 @@
+package com.ymmihw.checkstyle;
+
+import static java.util.Collections.*;
+
+public class StarImport {
+  public static void main(String[] args) {
+    System.out.println(emptyList());
+  }
+}
